13154849e935913a852fc71a51dc874892de6afd
Yoav Danieli <yoav.danieli91@gmail.com>
2022-06-23 22:57:20 +0700

    DAO

---
+++ sophmore/DAO/hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x01864fE03B51DC04c94F76AB74d0a1a36a031392";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
